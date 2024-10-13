e41d0189c0d755910614bece059d6935500a934a
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2024-10-13 19:30:08 -0300

    marcando tarefa como concluída

--- src/components/Tarefa/styles.ts
+++ src/components/Tarefa/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Botao } from '../../styles'
 import variaveis from '../../styles/variaveis'
 
 import * as enums from '../../utils/enums/Tarefa'
@@ -28,12 +29,18 @@ export const Card = styled.div`
   padding: 16px;
   margin-bottom: 32px;
   border-radius: 16px;
+
+  label {
+    display: flex;
+    align-itens: center;
+    margin-bottom: 16px;
+  }
 `
 
 export const Titulo = styled.h3`
   font-weight: bold;
   font-size: 18px;
-  margin-bottom: 16px;
+  margin-left: 8px;
 `
 
 export const Tag = styled.span<TagProps>`
@@ -64,18 +71,6 @@ export const Descricao = styled.textarea`
 export const BarraAcoes = styled.div`
   border-top: 1px solid rgba(0, 0, 0, 0.1);
   padding-top: 16px;
-`
-
-export const Botao = styled.button`
-  font-weight: bold;
-  font-size: 12px;
-  color: #fff;
-  padding: 8px 12px;
-  border: none;
-  cursor: pointer;
-  background-color: #2f3640;
-  border-radius: 8px;
-  margin-right: 8px;
 `
 
 export const BotaoCancelarRemover = styled(Botao)`
